f2f694b2233e1f9bb260b21a341932cbc0f1f756
ArturoDelMadrazo <33378667+Ischyrosmilus@users.noreply.github.com>
2024-03-04 13:03:50 +0100

    Add card

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,22 @@
 
 import ContainerButton from './components/ContainerButton.vue';
 import Td from './components/VueConteneur/Td.vue';
+import Card from './components/Card.vue';
 
 </script>
 <template>
 	
 <ContainerButton class="newContainer">Salut</ContainerButton>
 <Td class="test">Coucou</Td>
+<div id="index">
+    <nav class="nav-bar">
+        <ContainerButton class="newContainer">Nouveau conteneur</ContainerButton>
+    </nav>
+
+    <Card>
+        Test coucou
+    </Card>
+
+</div>
 
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <h2><slot /></h2>
+        </div>
+        <div class="card-status">
+            <p><slot /></p>
+        </div>
+        <div class="card-actions">
+            <button class="start">Lancer</button>
+            <button class="stop">Stopper</button>
+            <button class="restart">Relancer</button>
+            <button class="delete">Supprimer</button>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
