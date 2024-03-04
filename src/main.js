678869342c504f80bdf34733e14468063d74a69d
Administrator <admin@example.com>
2024-03-04 10:09:09 +0100

    feat(router)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import * as VueRouter from 'vue-router';
+import Home from './pages/HomePage.vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+const app = createApp(App);
+
+const About = { template: '<div>About</div>' }
+
+const routes = [
+  { path: '/', component: Home},
+  { path: '/about', component: About}
+];
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+});
+
+app.use(router);
+
+app.mount('#app');
